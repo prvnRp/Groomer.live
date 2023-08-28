0c942f483dbdf43fb8775f62fa9e6aa341ab650a
SahithyTumma <tummasahithy@gmail.com>
2023-08-28 15:01:12 +0530

    1

--- src/Home.js
+++ src/Home.js
@@ -5,32 +5,71 @@ import './Home.css'
 import { NavLink } from "react-router-dom";
 
 function Home() {
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        const { username, password } = event.target.elements;
+        let headersList = {
+            "Accept": "*/*",
+            // "User-Agent": "Thunder Client (https://www.thunderclient.com)",
+            "Content-Type": "application/json"
+        }
+
+        let bodyContent = JSON.stringify({
+            "username": "sahithy",
+            "password": "password"
+        });
+
+        let response = await fetch("http://127.0.0.1:8000/admin/login", {
+            method: "POST",
+            body: bodyContent,
+            headers: headersList
+        });
+
+        let data = await response.text();
+        console.log(data);
+
+        // const data = Array.from(event.target.elements)
+        //     .filter((input) => input.name).reduce((obj, input) => Object.assign(obj, { [input.name]: input.value }), {})
+        // const data = { "username": username.value, "password": password.value };
+        // console.log(data);
+
+        // console.log(Password.value);
+        // await fetch('http://127.0.0.1:8000/admin/login', {
+        //     method: 'POST',
+        //     headers: {
+        //         'Content-Type': 'application/json'
+        //     },
+        //     body: JSON.stringify(data)
+        // })
+        // console.log("clicked")
+    }
 
     return (
         <div id="containerhome">
 
-            <div class="box" id="bluebox">
+            <div className="box" id="bluebox">
                 <img src={logo} alt="Logo" className="logobig" />
             </div>
 
-            <div class="box" id="redbox">
-                <div style={{ display: "flex", flexDirection: "row", gap: "5vw" }}>
+            <div className="box" id="redbox">
+                <form style={{ display: "flex", flexDirection: "row", gap: "5vw" }} onSubmit={handleSubmit} >
                     <div>
                         <input
                             type="text"
-                            name="Username"
+                            name="username"
                             placeholder='UserName'
                         />
                     </div>
                     <div>
                         <input
                             type="text"
-                            name="Password"
+                            name="password"
                             placeholder='Password'
                         />
                     </div>
-                    <NavLink to="/admin" ><img src={login} alt="Logout" /></NavLink>
-                </div>
+                    <input type="submit" value="Login" style={{ backgroundImage: "url(/images/login.svg)" }} />
+                    <img src={login} alt="Logout" />
+                </form>
             </div>
 
         </div>
